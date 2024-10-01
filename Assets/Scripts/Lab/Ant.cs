2e1ce3bfbab5cc1898026b10b6ed16860ea74361
Artori4z <kongphop.buch@bumail.net>
2024-10-01 16:12:13 +0700

    .

--- Assets/Scripts/Lab/Ant.cs
+++ Assets/Scripts/Lab/Ant.cs
@@ -5,8 +5,37 @@ using UnityEngine;
 
 public class Ant : Enemy
 {
-  public override void Behaviour() 
+    [SerializeField] private Vector2 velocity;
+    [SerializeField] private Transform[] movePoints;
+    private void Start()
     {
-        Debug.Log("Ant.Behaviour");
+        Init(10);
+        Debug.Log("Ant health " + Health);
+        
+    }
+    public void FixedUpdate()
+    {
+        Behaviour();
+    }
+    public override void Behaviour() 
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+
+        if (rb.position.x <= movePoints[0].position.x && velocity.x < 0)
+        {
+            Flip();
+        }
+        else if (rb.position.x >= movePoints[1].position.x && velocity.x > 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        velocity *= -1;
+        Vector2 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
